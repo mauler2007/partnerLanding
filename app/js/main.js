aa0daf041450da4d251c6438e39bd5778ec7a76c
Your Namemauler2007 <mauler2007@gmail.com>
2021-08-28 18:53:51 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,189 @@
+$(function () {
+
+
+  //  LOCALISATION  LOCALISATION  LOCALISATION
+  const swhithLang = document.querySelectorAll('.langCheck__item:not(.selected) input');
+
+  fetch('localisation/localisation.json')
+    .then(response => {
+      if (response.status !== 200) {
+        return Promise.reject(new Error(response.statusText))
+      }
+      return Promise.resolve(response)
+    })
+    .then(response => response.json())
+    .then(data => {
+      const localis = data,
+        localArr = ['ru', 'tr', 'pt'],
+        langDisplay = document.querySelector('.langCheck__item.selected'),
+        langDisplayFlag = langDisplay.querySelector('.icon'),
+        langDisplayContext = langDisplay.querySelector('.langContext'),
+        languageHref = window.location.hash;
+
+      if (languageHref.length != 0) {
+        console.log('111');
+
+        language = languageHref.substr(1);
+        langDisplay.className = `langCheck__item langCheck__item-${language} selected`;
+        langDisplayFlag.className = `icon icon-${language}`;
+        langDisplayContext.innerHTML = language;
+        document.querySelectorAll('.langCheck__item:not(.selected)').forEach(item => {
+          if (item.classList.contains(`langCheck__item-${language}`)) {
+            item.classList.add('active');
+          } else {
+            item.classList.remove('active');
+          }
+
+        })
+      } else if (language.length != 0 && localArr.includes(language)) {
+        console.log('222');
+
+        langDisplay.className = `langCheck__item langCheck__item-${language} selected`;
+        langDisplayFlag.className = `icon icon-${language}`;
+        langDisplayContext.innerHTML = language;
+        document.querySelectorAll('.langCheck__item:not(.selected)').forEach(item => {
+          if (item.classList.contains(`langCheck__item-${language}`)) {
+            item.classList.add('active');
+          } else {
+            item.classList.remove('active');
+          }
+
+        })
+      }
+
+      for (let key in localis) {
+        let string = document.querySelectorAll('.lng-' + key),
+          stringLocalis = localis[key][language];
+
+        if (string && stringLocalis) {
+          for (let elem of string) {
+            elem.innerHTML = localis[key][language];
+          }
+
+        }
+      }
+    })
+    .catch(error => console.log('error', error))
+
+
+  //  LOCALISATION  LOCALISATION  LOCALISATION
+
+
+  //  SWHITCH LOCALISATION  SWHITCH LOCALISATION  SWHITCH LOCALISATION
+
+  swhithLang.forEach(item => {
+    item.addEventListener('change', () => {
+      let val = item.name;
+      window.location.href = window.location.pathname + '#' + val;
+      location.reload();
+      language = val;
+    })
+  })
+
+    //  SWHITCH LOCALISATION  SWHITCH 
+  //  Определяем поддержку  браузером webp  для загрузки нужных форматов изображений
+
+  // function testWebP(callback) {
+  //   var webP = new Image();
+  //   webP.onload = webP.onerror = function () {
+  //     callback(webP.height == 2);
+  //   };
+  //   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  // }
+
+  // testWebP(function (support) {
+
+  //   if (support == true) {
+  //     document.querySelector('body').classList.add('webp');
+  //   } else {
+  //     document.querySelector('body').classList.add('no-webp');
+  //   }
+  // });
+
+  // one = document.getElementById('instructionNumberOne');
+  // two = document.getElementById('instructionNumberTwo');
+  // three = document.getElementById('instructionNumberThree');
+  // four = document.getElementById('instructionNumberFour');
+
+
+  // var one = document.getElementById('instructionNumberOne');
+  // var two = document.getElementById('instructionNumberTwo');
+  // var three = document.getElementById('instructionNumberThree');
+  // var four = document.getElementById('instructionNumberFour');
+
+  
+  
+      // $(window).scroll(function () {
+      //   ($(window).scrollTop() > 10);
+  
+      //   setTimeout(function () {
+  
+      //     one.classList.add("active");
+  
+      //   }, 50);
+  
+      //   setTimeout(function () {
+  
+      //     two.classList.add("active");
+  
+      //   }, 250);
+  
+      //   setTimeout(function () {
+  
+      //     three.classList.add("active");
+  
+      //   }, 450);
+      //   setTimeout(function () {
+  
+      //     four.classList.add("active");
+  
+      //   }, 650);
+  
+      // });
+
+
+
+
+  // document.addEventListener("mousemove", parallax);
+  // const elem = document.querySelector("#parallax");
+  // // Magic happens here
+  // function parallax(e) {
+  //   let _w = window.innerWidth / 2;
+  //   let _h = window.innerHeight / 2;
+  //   let _mouseX = e.pageX;
+  //   let _mouseY = e.pageY;
+  //   let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY + _h) * 0.01}%`;
+  //   let x = `${_depth1}`;
+  //   console.log(x);
+  //   elem.style.backgroundPosition = x;
+  // }
+
+
+
+  // document.addEventListener("mousemove", parallax);
+  // function parallax(e) {
+  //   this.querySelectorAll('.layer').forEach(layer => {
+  //     const speed = layer.getAttribute('data-speed')
+
+  //     const x = (window.innerWidth - e.pageX*speed/100)
+  //     const y = (window.innerWidth - e.pageY*speed/100)
+
+  //     layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+  //   })
+  // }
+
+
+  // ThisIsWebP().then(function () {
+  //   // var getWebp = document.getElementById("parallax");
+  //   // getWebp.addClass('webp');
+  //   console.log('Nowebp')//Нет поддержки webp
+
+  // }, function () {
+  //   console.log('Nowebp')//Нет поддержки webp
+  // });
+
+
+
+
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
+
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+
+const pugRender = require('gulp-pug');
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    },
+    notify: false
+  })
+}
+
+function pug() {
+  return src('app/pug/*.pug')
+  .pipe(pugRender({
+    pretty: true
+  }))
+  .pipe(dest('app'))
+  .pipe(browserSync.stream())
+}
+
+
+function styles() {
+  return src('app/scss/**/*.scss')
+    .pipe(scss({
+      outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      add: true,
+      grid: false
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    // .pipe(concat('style.min.css'))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+}
+
+function scripts() {
+  return src([
+      'node_modules/jquery/dist/jquery.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+}
+
+function images() {
+  return src('app/images/**/*.*')
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      })
+      // imagemin.svgo({
+      //     plugins: [
+      //         {removeViewBox: true},
+      //         {cleanupIDs: false}
+      //     ]
+      // })
+    ]))
+    .pipe(dest('dist/images'))
+}
+
+function build() {
+  return src([
+      'app/*.html',
+      // 'app/css/style.min.css',
+      'app/css/**/*.min.css',
+      'app/js/main.min.js'
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'))
+}
+
+function cleanDist() {
+  return del('dist')
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/pug/**/*.pug'], pug);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.pug = pug;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build);
+
+exports.default = parallel(pug,styles, scripts, browsersync, watching);
