fb8506375d0f21e829d1d1b564b682baaa897ff0
Your Namemauler2007 <mauler2007@gmail.com>
2021-08-29 18:58:58 +0300

    add adv banner tools

--- app/js/main.js
+++ app/js/main.js
@@ -81,6 +81,21 @@ $(function () {
   })
 
     //  SWHITCH LOCALISATION  SWHITCH 
+
+    //  класс для хедера при скроле
+
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  /*Если сделали скролл на 100px задаём новый класс для header*/
+  if (height > 100) {
+    $('.header').addClass('header--fixed');
+  } else {
+    /*Если меньше 100px удаляем класс для header*/
+    $('.header').removeClass('header--fixed');
+  }
+});
+
+
   //  Определяем поддержку  браузером webp  для загрузки нужных форматов изображений
 
   // function testWebP(callback) {
